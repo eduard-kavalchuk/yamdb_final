12498030dc098e4866fb74b157ccce58a7c2c4fb
eduard-kavalchuk <eduardkovalchuk@mail.ru>
2022-12-22 23:10:08 +0300

    flake8

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,9 +1,9 @@
-from django.urls import include, path
-from rest_framework.routers import DefaultRouter
-
 from api.views import (CategoryViewSet, CommentViewSet, GenreViewSet,
                        ReviewViewSet, TitleViewSet, UserViewSet, get_token,
                        signup)
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
 
 router_v1 = DefaultRouter()
 router_v1.register('categories', CategoryViewSet, basename='categories')
