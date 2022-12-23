12092b51aeebc76bef094f6ce1580c20e10d4fcb
eduard-kavalchuk <eduardkovalchuk@mail.ru>
2022-12-23 22:43:45 +0300

    Add default values to settings.py

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -65,10 +65,10 @@ DATABASES = {
             'DB_ENGINE', default='django.db.backends.postgresql'
         ),
         'NAME': os.getenv('DB_NAME', default='postgres'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres2'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres3'),
+        'HOST': os.getenv('DB_HOST', default='db'),
+        'PORT': os.getenv('DB_PORT', default=5432),
     }
 }
 
