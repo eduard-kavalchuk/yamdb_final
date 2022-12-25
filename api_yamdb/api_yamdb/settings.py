915d566566db6e31bbd7b2f58b8c4a32c503e133
eduard-kavalchuk <eduardkovalchuk@mail.ru>
2022-12-25 19:54:13 +0300

    Fix workflow file

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DJANGO_DEBUG')
+DEBUG = os.getenv('DGB')
 
 ALLOWED_HOSTS = ['*']
 
